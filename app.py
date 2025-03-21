5d1c8245e0bf88feab70891035c818c2e97b4202
Weynio-Santana <weyniovierasantana@gmail.com>
2025-03-21 09:37:38 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import sqlite3 
+from flask import Flask, request, jsonify 
+
+
+app = Flask(__name__)
+
+
+@app.route("/femandaopix")
+def manda_o_pix():
+    
+    return "<h2>SE TEM DOR DE CUTUVELO, TÁ DEVENDO</h2>"
+
+
+
+def init_db():
+   
+    with sqlite3.connect("database.db") as conn:
+        
+        conn.execute(
+            """
+                CREATE TABLE IF NOT EXISTS LIVROS(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    titulo TEXT NOT NULL,  
+                    categoria TEXT NOT NULL,  
+                    autor TEXT NOT NULL, 
+                    image_url TEXT NOT NULL  
+                )
+            """
+        ) 
+
+
+init_db()
+
+
+@app.route("/doar", methods=["POST"])
+def doar():
+    
+    dados = request.get_json()
+    print(f" AQUI ESTÃO OS DADOS RETORNADOS DO CLIENTE {dados}")  
+
+   
+    titulo = dados.get("titulo")  
+    categoria = dados.get("categoria")  
+    autor = dados.get("autor") 
+    image_url = dados.get("image_url")  
+
+  
+    if not titulo or not categoria or not autor or not image_url:
+        return jsonify({"erro": "Todos os campos são obrigatórios"}), 400  
+    
+
+    with sqlite3.connect("database.db") as conn:
+
+        conn.execute(f"""
+        INSERT INTO LIVROS (titulo, categoria, autor, image_url) 
+        VALUES ("{titulo}", "{categoria}", "{autor}", "{image_url}")
+        """)  
+    
+    conn.commit() 
+
+   
+    return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
+
+if __name__ == "__main__":
+   
+    app.run(debug=True)
